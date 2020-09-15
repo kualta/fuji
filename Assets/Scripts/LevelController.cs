f97876416ba07ed2c7a0cf89ab81bc05d8792933
Andrew Fabelson <servghh91@protonmail.com>
2020-09-15 20:32:34 +0300

    Added PlatformController and LevelController

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    public GameController gameController;
+    public GameObject platform;
+
+    void Start() {
+        StartCoroutine(SpawnPlatform());
+    }
+
+
+    IEnumerator SpawnPlatform() {
+        Instantiate(platform, new Vector3(0f, -7f, 0f), Quaternion.identity);
+
+        yield return new WaitForSeconds(2 / gameController.levelNumber);
+        StartCoroutine(SpawnPlatform());
+    }
+}
---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    public GameController gameController;
+    public bool initialPlatform;
+
+
+    void Start() {
+        RandomizePosition();
+    }
+
+    void Update() {
+        Move();
+        CheckPosition();
+    }
+
+    void Move() {
+        transform.Translate(Vector3.up * Time.deltaTime * gameController.levelNumber, Space.World);
+    }
+
+    void RandomizePosition() {
+        transform.position += new Vector3(Random.Range(-2f, 1.5f), 0f, 0f);
+    }
+
+    void CheckPosition() {
+        if (transform.position.y > 7f) {
+            Destroy(this.gameObject);
+        }
+    }
+}
