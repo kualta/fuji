d0a8905a4133d2ea4f0175e3bd88d80a865822b1
Andrew Fabelson <servghh91@protonmail.com>
2020-09-15 11:43:07 +0300

    Added level switching

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,23 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    public int levelNumber;
+    public Sprite[] levelImages;
+
+    public static GameController Instance;
+     
+
     public void OnCredits() {
         SceneManager.LoadScene("Credits");
     }
 
     public void OnNextLevel() {
-
+        if (levelNumber < 7) {
+            levelNumber++;
+        }
+        UpdateLevelButton(levelNumber);
+        Debug.Log(levelNumber);
     }
 
     public void OnPreviousLevel() {
-
+        if (levelNumber > 1) {
+            levelNumber--;
+        }
+        UpdateLevelButton(levelNumber);
+        Debug.Log(levelNumber);
     }
 
     public void OnMenu() {
         SceneManager.LoadScene("Main Menu");
+    }
+
+    public void UpdateLevelButton(int number) {
+        Image levelButtonImage = GameObject.Find("Level").GetComponent<Image>();
+
+        levelButtonImage.sprite = levelImages[number];
+    }
+
+    public void UpdateLevelButton() {
+        Image levelButtonImage = GameObject.Find("Level").GetComponent<Image>();
+
+        levelButtonImage.sprite = levelImages[levelNumber];
+    }
+
+    void Start() {
+        LoadLastLevel();
+    }
+
+    void Awake() {
+        Instance = this;
+        UpdateLevelButton(levelNumber);
+    }
+
+    internal void LoadLastLevel() {
+
+        // FIXME: This is not an expected behaviour.
+        // A real implementation is needed.
+
+        levelNumber = 1;
     }
 }
---
+++ Assets/Scripts/MovingText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingText : MonoBehaviour
+{
+
+    void Start() {
+        StartCoroutine(ShrinkObject());
+    }
+
+    IEnumerator ShrinkObject() {
+        for (int i = 0; i < 20; i++) {
+            transform.localScale *= .99f;
+            yield return new WaitForSeconds(.05f);
+        }
+        StartCoroutine(ExpandObject());
+    }
+
+    IEnumerator ExpandObject() {
+        for (int i = 0; i < 20; i++) {
+            transform.localScale /= .99f;
+            yield return new WaitForSeconds(.05f);
+        }
+        StartCoroutine(ShrinkObject());
+    }
+}
